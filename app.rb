e2567db9f38eb44e88a8630821dbab7a17e6114f
joao <joaoxavier30@gmail.com>
2019-12-06 13:22:57 -0600

    ponle lo que tu quieras

--- app.rb
+++ app.rb
@@ -13,6 +13,6 @@ end
 post "/probar" do
     comenzarAjugar = ComenzarAjugar.new "8"
     letraAprobar = params["letraAProbar"]
-    session["Adivinando"] = comenzarAjugar.contarcasillas
+    session["Adivinando"] = comenzarAjugar.contarcasillas letraAprobar
     erb(:index)
 end
