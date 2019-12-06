f0f97c590551af237f768c77741c1d7c08388078
scarlett07 <scarlett.geo@gmail.com>
2019-12-06 12:45:30 -0500

    Feature casillas

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,3 +6,16 @@ Given(/^Entro a la pagina$/) do
   Then(/^debo ver "([^"]*)"$/) do |nombreBanda|
     expect(page.body).to match /#{nombreBanda}/m
 end
+
+Given(/^Mientras este en la pagina de juego$/) do
+  visit '/'
+end
+
+Given(/^que uso la cancion predeterminda$/) do
+  visit '/'
+end
+
+
+Then(/^deben aparecer  "([^"]*)"$/) do |numeroEspacios|
+	expect(page.body).to match /#{numeroEspacios}/m
+end
--- features/visualizarCasillas.feature
+++ features/visualizarCasillas.feature
@@ -1,5 +1,5 @@
 Feature: VerCasillas
 Scenario: Debe tener las casillas para ingresar las letras
-	Given que la banda es Queen
-	When la cancion sea  "Don't stop me now"
-	Then deben aparecer 14 casillas para ingresar las letras.
+	Given Mientras este en la pagina de juego
+	And que uso la cancion predeterminda
+	Then deben aparecer  "--------------"
