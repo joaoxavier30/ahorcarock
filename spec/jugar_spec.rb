82c361f6bc0d91cfc4abd580a46c96cca664e7c2
scarlett07 <scarlett.geo@gmail.com>
2019-12-06 14:06:27 -0500

    fix rspec

--- spec/jugar_spec.rb
+++ spec/jugar_spec.rb
@@ -2,13 +2,13 @@ require './lib/ComenzarAjugar'
 
 describe ComenzarAjugar do
     it "muestraBandaPorDefecto Queen" do
-      comenzarAjugar = ComenzarAjugar.new()
+      comenzarAjugar = ComenzarAjugar.new "8"
       nombreBanda = comenzarAjugar.nombreBanda
       expect(nombreBanda).to eq "Queen"
     end
 
     it "muestraBandaPorDefecto Queen" do
-      comenzarAjugar = ComenzarAjugar.new()
+      comenzarAjugar = ComenzarAjugar.new "8"
       casillas = comenzarAjugar.contarcasillas
       expect(casillas).to eq "--------------"
     end  
